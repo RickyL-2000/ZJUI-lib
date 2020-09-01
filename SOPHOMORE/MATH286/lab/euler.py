683c55b87b009f5e7da5131c60c920cdfa9340b2
RickyL-2000 <15305813731@163.com>
2020-09-01 23:08:29 +0800

    clean the structure

--- SOPHOMORE/MATH286/lab/euler.py
+++ SOPHOMORE/MATH286/lab/euler.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import os
+from .analysis import analyse
 
 base_dir = os.getcwd() + "/SOPHOMORE/MATH286/lab"
 
@@ -12,28 +13,6 @@ def f1(t, y):
 
 def f2(t, y):
     return y*y*y + t*y*y + t*t*y + t*t*t
-
-# %%
-def analyse(f, method, a, b, t0, y0, h=(0.01, 0.005, 0.001)):
-    """
-    The main process to analyse a set of results with different step lengths
-    :param f: the f function of the IVP
-    :param method: the numerical method
-    :param a: left bound
-    :param b: right bound
-    :param t0: initial t
-    :param y0: initial y
-    :param h: a list of step lengths to be analysed
-    :return: a DataFrame of numerical results
-    """
-    df = pd.DataFrame()
-    space = h[0]
-    df['t'] = np.linspace(a, b, round((b - a) / space) + 1)
-    for i in range(len(h)):
-        t, y = method(f, a, b, t0, y0, h[i])
-        df['y with h='+str(h[i])] = [y[j] for j in range(0, len(y), round(space / h[i]))]
-
-    return df
 
 # %%
 def euler_explicit(f, a, b, t0, y0, h):
