d329f320f4314b4923356d5cc99095eb3630c427
RickyL-2000 <15305813731@163.com>
2020-09-01 22:05:59 +0800

    精简了分析过程

--- SOPHOMORE/MATH286/lab/euler.py
+++ SOPHOMORE/MATH286/lab/euler.py
@@ -11,6 +11,28 @@ def f2(t, y):
     return y*y*y + t*y*y + t*t*y + t*t*t
 
 # %%
+def analyse(f, method, a, b, t0, y0, h=(0.01, 0.005, 0.001)):
+    """
+    The main process to analyse a set of results with different step lengths
+    :param f: the f function of the IVP
+    :param method: the numerical method
+    :param a: left bound
+    :param b: right bound
+    :param t0: initial t
+    :param y0: initial y
+    :param h: a list of step lengths to be analysed
+    :return: a DataFrame of numerical results
+    """
+    df = pd.DataFrame()
+    space = h[0]
+    df['t'] = np.linspace(a, b, round((b - a) / space) + 1)
+    for i in range(len(h)):
+        t, y = method(f, a, b, t0, y0, h[i])
+        df['y with h='+str(h[i])] = [y[j] for j in range(0, len(y), round(space / h[i]))]
+
+    return df
+
+# %%
 def euler_explicit(f, a, b, t0, y0, h):
     assert a <= t0 <= b
     t_list = []
@@ -48,58 +70,19 @@ def euler_explicit(f, a, b, t0, y0, h):
 
 # %%
 # 确定 f1 的区间
-df1 = pd.DataFrame()
-a1 = -1.0
+a1 = -3.0
 b1 = 1.0
-df1['t'] = np.linspace(a1, b1, int((b1 - a1) / 0.1 + 1))
-t1_1, y1_1 = euler_explicit(f1, a1, b1, 0, 1, 0.1)
-t1_2, y1_2 = euler_explicit(f1, a1, b1, 0, 1, 0.01)
-t1_3, y1_3 = euler_explicit(f1, a1, b1, 0, 1, 0.001)
-df1['y with h=0.1'] = [y1_1[i] for i in range(0, len(y1_1), round(0.1 / 0.1))]
-df1['y with h=0.01'] = [y1_2[i] for i in range(0, len(y1_2), round(0.1 / 0.01))]
-df1['y with h=0.001'] = [y1_3[i] for i in range(0, len(y1_3), round(0.1 / 0.001))]
+df1 = analyse(f1, euler_explicit, a1, b1, 0, 1, h=(0.01, 0.005, 0.001))
 
 # %%
-plt.plot(t1_1, y1_1)
-plt.show()
-plt.plot(t1_2, y1_2)
-plt.show()
-plt.plot(t1_3, y1_3)
-plt.show()
-
-# %%
-print("euler h = 0.1: ", y1_1[-1])
-print("euler h = 0.01: ", y1_2[-1])
-print("euler h = 0.001: ", y1_3[-1])
-
-# %%
+pd.set_option('display.max_rows', None)
 print(df1)
 
 # %%
 # 确定 f2 的区间
-df2 = pd.DataFrame()
-a2 = -1.0
-b2 = 1.0
-df2['t'] = np.linspace(a2, b2, int((b2 - a2) / 0.1 + 1))
-t2_1, y2_1 = euler_explicit(f2, a2, b2, 0, 1, 0.1)
-t2_2, y2_2 = euler_explicit(f2, a2, b2, 0, 1, 0.01)
-t2_3, y2_3 = euler_explicit(f2, a2, b2, 0, 1, 0.001)
-df2['y with h=0.1'] = [y2_1[i] for i in range(0, len(y2_1), round(0.1 / 0.1))]
-df2['y with h=0.01'] = [y2_2[i] for i in range(0, len(y2_2), round(0.1 / 0.01))]
-df2['y with h=0.001'] = [y2_3[i] for i in range(0, len(y2_3), round(0.1 / 0.001))]
-
-# %%
-plt.plot(t2_1, y2_1)
-plt.show()
-plt.plot(t2_2, y2_2)
-plt.show()
-plt.plot(t2_3, y2_3)
-plt.show()
-
-# %%
-print("euler h = 0.1: ", y2_1[-1])
-print("euler h = 0.01: ", y2_2[-1])
-print("euler h = 0.001: ", y2_3[-1])
+a2 = -30.0
+b2 = 5.0
+df2 = analyse(f2, euler_explicit, a2, b2, 0, 1, h=(0.01, 0.005, 0.001))
 
 # %%
 print(df2)
