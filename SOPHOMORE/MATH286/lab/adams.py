precision bug fixed

SOPHOMORE/MATH286/lab/adams.py
@@ -42,7 +42,7 @@ def lin_multistep(f, a, b, t0, y0, h, **kwargs) -> Tuple[np.ndarray, np.ndarray]
     """
 
     def __update(t_list: List, y_list: List, f_list: List, f, h: float, k: int, alpha: Tuple, beta: Tuple,
-                 threshold=1e-4, epochs=100) -> float:
+                 threshold=1e-6, epochs=100) -> float:
         """
         Choose to whether update the y_{i+1} explicitly or implicitly
 
@@ -98,7 +98,7 @@ def lin_multistep(f, a, b, t0, y0, h, **kwargs) -> Tuple[np.ndarray, np.ndarray]
     if 'threshold' in kwargs:
         threshold = kwargs['threshold']
     else:
-        threshold = 1e-4
+        threshold = 1e-6
     if 'epochs' in kwargs:
         epochs = kwargs['epochs']
     else:
@@ -178,7 +178,7 @@ def adams_monlton(f, a, b, t0, y0, h, **kwargs) -> Tuple[np.ndarray, np.ndarray]
     if 'threshold' in kwargs:
         threshold = kwargs['threshold']
     else:
-        threshold = 1e-4
+        threshold = 1e-10
     if 'epochs' in kwargs:
         epochs = kwargs['epochs']
     else:

SOPHOMORE/MATH286/lab/euler.py
@@ -80,7 +80,7 @@ def euler_explicit(f, a, b, t0, y0, h) -> Tuple[np.ndarray, np.ndarray]:
     return np.array(t_list), np.array(y_list)
 
 # %%
-def euler_implicit(f, a, b, t0, y0, h, threshold=1e-4, epochs=100) -> Tuple[np.ndarray, np.ndarray]:
+def euler_implicit(f, a, b, t0, y0, h, threshold=1e-6, epochs=100) -> Tuple[np.ndarray, np.ndarray]:
     """
     Implicit (backward) Euler Method
 
@@ -132,7 +132,7 @@ def euler_implicit(f, a, b, t0, y0, h, threshold=1e-4, epochs=100) -> Tuple[np.n
     return np.array(t_list), np.array(y_list)
 
 # %%
-def euler_trapezium(f, a, b, t0, y0, h, threshold=1e-4, epochs=50) -> Tuple[np.ndarray, np.ndarray]:
+def euler_trapezium(f, a, b, t0, y0, h, threshold=1e-6, epochs=50) -> Tuple[np.ndarray, np.ndarray]:
     """
     Implicit (backward) Euler Method
 