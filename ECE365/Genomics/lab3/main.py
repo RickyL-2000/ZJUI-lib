11b7cd62f5ea6d43e5cb9e4c27ca27be40a8f1ac
RickyL-2000 <ruiqili4@illinois.edu>
2021-03-24 02:44:40 +0800

    365 G lab3 partial

--- ECE365/Genomics/lab3/main.py
+++ ECE365/Genomics/lab3/main.py
@@ -11,7 +11,21 @@ class Lab3(object):
         Output - You should return the 53 x 3902 dataframe
         '''
         #start code here
-    
+
+        df = pd.DataFrame()
+        for each in snp_lines:
+            line = each.strip().split('\t')
+            col_name = line[0] + ':' + line[1]
+            line = line[9:]
+            col = np.zeros(53)
+            for i in range(53):
+                if line[i][0] == '.':
+                    col[i] = np.nan
+                else:
+                    col[i] = int(line[i][0]) + int(line[i][2])
+            df[col_name] = col
+
+        return df
         #end code here
 
     def create_target(self,header_line) :
@@ -20,7 +34,16 @@ class Lab3(object):
         Output - a list of values(either 0 or 1)
         '''
         #start code here
-        
+
+        line = header_line.strip().split('\t')[9:]
+        ret = []
+        for dog in line:
+            if dog[:4] == 'dark':
+                ret.append(0)
+            elif dog[:6] == 'yellow':
+                ret.append(1)
+        return ret
+
         #end code here
     
     def logistic_reg_per_snp(self,df) :
@@ -30,6 +53,16 @@ class Lab3(object):
         '''
         #start code here
         
+        pvalues = []
+        betavalues = []
+        col_names = df.columns.tolist()[:-1]
+        for i in range(len(col_names)):
+            clf = sm.Logit(df['target'], sm.add_constant(df[col_names[i]]), missing='drop')
+            res = clf.fit(method='bfgs', disp=False)
+            betavalues.append(round(res.params[1], 5))
+            pvalues.append(round(res.pvalues[1], 9))
+
+        return pvalues, betavalues
         #end code here
     
     
